a03f97c5211f253d1f57e89ae5ac551a2e4b66b0
Krishna Paudel <krisnachhetri07@gmail.com>
2022-09-02 00:36:09 +1000

    comit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import Nav from "./Components/Navbar/Nav";
 import "./App.css";
-import Home from "./Components/HomeItems/Home";
-import Sponsers from "./Components/HomeItems/Sponsers";
+
 import Section3 from "./Components/Section3/Section3";
+import Section4 from "./Components/Section4/Section4";
+import Section1 from "./Components/Section1/Section1";
+import Section5 from "./Components/Section5/Section5";
+import Section6 from "./Components/Section6/Section6";
+import ResueableContents from "./Components/ResueableContents";
 
 function App() {
   return (
     <div className="App">
-      <div className="topStripe"></div>
-
-      <div className="bodyContainer">
-        <div className="hrItems">
-          <span />
-          <span />
-          <span />
-        </div>
-        <Nav />
-        <Home />
-        <Sponsers />
+      <div>
+        <div className="topStripe"></div>
+        <Section1 />
       </div>
+
       <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <div
+        style={{
+          width: "100%",
+          position: "relative",
+          background: "white",
+          padding: "12rem",
+          zIndex: "10",
+          border: "1px solid red",
+        }}
+      >
+        <ResueableContents
+          topics="Virtual event"
+          headings="Watch Sessions on demand"
+          des="Stripe Sessions—our annual conference—is now available to watch on demand. Learn more about the future of the internet economy and see new Stripe products in action."
+          btnText={"Watch now"}
+          Color={"blue"}
+          textColor="white"
+        />
+      </div>
+      <Section5 />
+      <Section4 />
     </div>
   );
 }
 
 export default App;
+
+//
